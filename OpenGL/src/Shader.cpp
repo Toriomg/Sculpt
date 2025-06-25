c4d90683fff9c47a5d6abc6f0eef7cc086bbebca
Toriomg <hectorusuario@gmail.com>
2025-06-26 01:38:44 +0200

    Shader uniforms minor fixed

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -110,11 +110,11 @@ void Shader::SetUniformMat4f(const std::string& name, const glm::mat4& matrix) {
 	GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0])); // Set a 4x4 matrix uniform variable in the shader
 }
 
-int Shader::GetUniformLocation(const std::string& name) {
+GLint Shader::GetUniformLocation(const std::string& name) const{
 	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end()) {
 		return m_UniformLocationCache[name]; // Return cached location if it exists
 	}
-	GLCall(int location = glGetUniformLocation(m_RendererID, name.c_str()));
+	GLCall(GLint location = glGetUniformLocation(m_RendererID, name.c_str()));
 	if (location == -1) {
 		std::cerr << "Warning: uniform '" << name << "' doesn't exist!" << std::endl;
 	}
