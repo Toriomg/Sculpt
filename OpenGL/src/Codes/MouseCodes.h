8460d3237a561604b8785afefd45af3b1bc2d0cd
Toriomg <hectorusuario@gmail.com>
2025-06-27 02:27:33 +0200

    Codes for keys and mouse

--- OpenGL/src/Codes/MouseCodes.h
+++ OpenGL/src/Codes/MouseCodes.h
@@ -1,1 +1,15 @@
 #pragma once
+
+// From glfw3.h
+#define CODE_MOUSE_BUTTON_1         0
+#define CODE_MOUSE_BUTTON_2         1
+#define CODE_MOUSE_BUTTON_3         2
+#define CODE_MOUSE_BUTTON_4         3
+#define CODE_MOUSE_BUTTON_5         4
+#define CODE_MOUSE_BUTTON_6         5
+#define CODE_MOUSE_BUTTON_7         6
+#define CODE_MOUSE_BUTTON_8         7
+#define CODE_MOUSE_BUTTON_LAST      CODE_MOUSE_BUTTON_8
+#define CODE_MOUSE_BUTTON_LEFT      CODE_MOUSE_BUTTON_1
+#define CODE_MOUSE_BUTTON_RIGHT     CODE_MOUSE_BUTTON_2
+#define CODE_MOUSE_BUTTON_MIDDLE    CODE_MOUSE_BUTTON_3
