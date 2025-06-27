0cd0abc6f67c9f4780b3f15f8c0568ef808b2bc8
Toriomg <hectorusuario@gmail.com>
2025-06-27 02:21:05 +0200

    Events copied

---
+++ OpenGL/src/Core.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Macro to assert conditions, triggering a breakpoint if false
+#define ASSERT(x) if (!(x)) __debugbreak() 
+// Macro to clear OpenGL errors before and after a function call
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__)) 
+// Macro to log OpenGL errors
+#define BIT(x) (1 << (x))
