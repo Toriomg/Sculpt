7591710700d815dbde6b3b070ce766b8a5b48afc
Toriomg <hectorusuario@gmail.com>
2025-07-09 20:41:08 +0200

    Application and main divided

---
+++ OpenGL/src/Core/Application.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+#include "../Graphics/Renderer.h"
+#include "../tests/tests.h" // For test::Test and test::TestMenu
+
+// Forward declare GLFWwindow to avoid including glfw3.h in the header.
+// This is good practice to keep headers clean and reduce compile times.
+struct GLFWwindow;
+
+class Application
+{
+public:
+    Application(const std::string& name = "3d MODELER",
+        unsigned int width = 1470, unsigned int height = 810);
+    ~Application();
+
+    void Run();
+
+private:
+    void Cleanup(); // Helper function for destructor
+
+private:
+    GLFWwindow* m_Window;
+    std::string m_AppName;
+    unsigned int m_Width, m_Height;
+
+    Renderer m_Renderer;
+
+    // Test framework members
+    test::Test* m_CurrentTest;
+    test::TestMenu* m_TestMenu;
+};
