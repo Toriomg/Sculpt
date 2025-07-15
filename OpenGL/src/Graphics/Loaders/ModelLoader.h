24755bfa08472ce08afe359311c0aa9c790ee5b1
Toriomg <hectorusuario@gmail.com>
2025-07-15 18:55:43 +0200

    A 'mesh' is rendered

--- OpenGL/src/Graphics/Loaders/ModelLoader.h
+++ OpenGL/src/Graphics/Loaders/ModelLoader.h
@@ -7,14 +7,9 @@
 #include <vector>
 #include <memory>
 
-class Vertex;
-class Mesh;
-class Material;
+#include "../Geometry/Vertex.h"
+#include "../Geometry/Mesh.h"
+#include "../Shading/Material.h"
 
-static class ModelLoader {
-private:
-	ModelLoader() = delete; // Prevent instantiation
-	static void parseFile(std::string& filepath, std::vector<Vec3>& temp_positions, std::vector<Vec2>& temp_tex_coords, std::vector<Vec3>& temp_normals, std::vector<std::string>& face_lines);
-public:
-	static Mesh LoadModel(std::string& filepath);
-};
+
+std::shared_ptr<Mesh> LoadModel(const std::string& filepath);
