c454c7c62a30ab37246efc7c25530840cb5d15d7
Toriomg <hectorusuario@gmail.com>
2025-07-15 14:24:24 +0200

    Some implementations on The Loader

--- OpenGL/src/Graphics/Geometry/Vertex.h
+++ OpenGL/src/Graphics/Geometry/Vertex.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "../Math/maths.h"
+
+class Vertex{
+public:
+	Vec3 position; // x, y, z
+	Vec2 texCoord; // u, v
+	Vec3 normal;   // nx, ny, nz
+};
--- OpenGL/src/Graphics/Loaders/ModelLoader.cpp
+++ OpenGL/src/Graphics/Loaders/ModelLoader.cpp
@@ -1,8 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+
 #include "ModelLoader.h"
 
 #include "../Geometry/Mesh.h"
 #include "../Shading/Material.h"
 
 bool ModelLoader::LoadModel(std::string& filepath, std::vector<std::shared_ptr<Mesh>>& outMeshes, std::vector<std::shared_ptr<Material>>& outMaterials) {
-	return true;
+	std::ifstream inputFile(filepath);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open the file." << std::endl;
+        return false; // Indicate an error
+    }
+
+    std::string line;
+    // Read the file line by line
+    while (std::getline(inputFile, line)) {
+        // Process each line
+        std::string firstWord = line.substr(0, line.find(" "));
+    }
+
+    inputFile.close();
+    return true;
 }
