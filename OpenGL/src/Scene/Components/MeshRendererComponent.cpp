4306d62cda73ff24cb9f965028b2f434e098f2c3
Toriomg <hectorusuario@gmail.com>
2025-07-30 01:53:46 +0200

    Start almost from scratch

--- OpenGL/src/Scene/Components/MeshRendererComponent.cpp
+++
@@ -1,9 +0,0 @@
-#include "MeshRendererComponent.h"
-
-MeshRendererComponent::MeshRendererComponent(
-    const std::shared_ptr<Mesh>& mesh,
-    const std::shared_ptr<Material>& material)
-    : m_Mesh(mesh), m_Material(material)
-{
-    // The member variables are initialized in the initializer list.
-}
