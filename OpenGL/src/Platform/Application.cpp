66750e5052ae37ca54c1d4fe6793dc8c14cb7d62
Toriomg <hectorusuario@gmail.com>
2025-07-30 02:47:10 +0200

    Log class, time and input remake

---
+++ OpenGL/src/Platform/Application.cpp
@@ -0,0 +1,71 @@
+#include "Application.h"
+
+
+Application::Application(const std::string& name, unsigned int width, unsigned int height)
+    : m_Window(nullptr), m_AppName(name), m_Width(width), m_Height(height)
+{
+    // Initialize the library
+    if (!glfwInit()) {
+        std::cerr << "ERROR: glfwInit() failed." << std::endl;
+        return;
+    }
+
+	// Set GLFW options
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    m_Window = glfwCreateWindow(m_Width, m_Height, m_AppName.c_str(), NULL, NULL);
+    if (!m_Window)
+    {
+        glfwTerminate();
+        std::cerr << "ERROR: glfwCreateWindow() failed." << std::endl;
+        return;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(m_Window);
+    glfwSwapInterval(1); // Enable vsync
+
+
+	// Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "ERROR: glewInit() failed." << std::endl;
+        return;
+    }
+
+
+    // Setup OpenGL State 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_BACK);
+
+    // Setup Time
+	Time::Init();
+
+    // Setup Input
+	InputManager::Get().Init(m_Window);
+}
+
+Application::~Application()
+{
+    glfwDestroyWindow(m_Window);
+    glfwTerminate();
+}
+
+void Application::Run()
+{
+    while (!glfwWindowShouldClose(m_Window))
+    {
+		InputManager::Get().Update();
+		Time::Update();
+
+		std::cout << InputManager::Get().GetMouseX() << std::endl;
+        glfwSwapBuffers(m_Window);
+        glfwPollEvents();
+    }
+}
