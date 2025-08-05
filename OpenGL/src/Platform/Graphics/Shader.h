812d755fd5f77e17336409629c28b2b989cf764a
Toriomg <hectorusuario@gmail.com>
2025-08-05 03:41:51 +0200

    More GAL readded

---
+++ OpenGL/src/Platform/Graphics/Shader.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <GL/glew.h> // Include GLEW for OpenGL function pointers
+
+#include "Platform/CoreUtils/Math/maths.h"
+#include "Platform/CoreUtils/Log.h"
+
+struct ShaderProgramSource {
+	std::string VertexSource; // Source code for the vertex shader
+	std::string FragmentSource; // Source code for the fragment shader
+};
+
+class Shader {
+private:
+	unsigned int m_RendererID;
+	std::string m_FilePath;
+	mutable std::unordered_map<std::string, GLint> m_UniformLocationCache; // Cache for uniform locations
+public:
+	Shader(const std::string& filepath);
+	Shader(const std::string& vertFilepath, const std::string& fragFilepath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	// Set uniform functions
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1ui(const std::string& name, unsigned int value);
+	void SetUniform1iv(const std::string& name, const int* array, int size);
+	void SetUniform1f(const std::string& name, float v);
+	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const Matx4f& matrix);
+private:
+	static std::string ReadFile(const std::string& filepath);
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CompileShader(const std::string& source, unsigned int type);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	GLint GetUniformLocation(const std::string& name) const;
+};
