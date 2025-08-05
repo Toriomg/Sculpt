4fdc7e58e2e9681e7e70f7b3c36d74455b29094a
Toriomg <hectorusuario@gmail.com>
2025-08-05 03:34:12 +0200

    Vertex buffer readded

---
+++ OpenGL/src/Platform/Graphics/Buffers/IndexBuffer.cpp
@@ -0,0 +1,30 @@
+#include "IndexBuffer.h"
+
+
+IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count)
+	: m_Count(count) // The count of indices IS the count passed in.
+{
+	CORE_LOG_ERROR(sizeof(unsigned int) == sizeof(GLuint)); // Good practice check
+
+	glGenBuffers(1, &m_RendererID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	// The total size in bytes is the element count * the size of one element.
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
+}
+
+IndexBuffer::~IndexBuffer()
+{
+	glDeleteBuffers(1, &m_RendererID); // Delete the buffEr
+}
+
+void IndexBuffer::Bind() const
+{
+	// Bind the vertex buffer to the GL_ARRAY_BUFFER target
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+}
+
+void IndexBuffer::Unbind() const
+{
+	// Unbind the vertex buffer by binding 0 to the GL_ARRAY_BUFFER target
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+} 
--- OpenGL/src/Platform/Graphics/Buffers/IndexBuffer.h
+++ OpenGL/src/Platform/Graphics/Buffers/IndexBuffer.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Platform/CoreUtils/Log.h"
+#include <GL/glew.h>
+
+class IndexBuffer {
+private:
+	unsigned int m_RendererID;
+	unsigned int m_Count;
+public:
+	IndexBuffer(const unsigned int* data, unsigned int count);// Constructor to create a Index buffer with given data and size
+	~IndexBuffer();// Destructor to clean up the Index buffer
+
+	void Bind() const;
+	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_Count; } // Returns the count of indices in the buffer
+};
