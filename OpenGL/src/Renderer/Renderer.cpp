df315ac5d7c7d08cc7dbb639547625be2f899f5c
Toriomg <hectorusuario@gmail.com>
2025-08-06 05:03:39 +0200

    More additions

---
+++ OpenGL/src/Renderer/Renderer.cpp
@@ -0,0 +1,57 @@
+#include "Renderer.h"
+#include "Material.h"
+#include "Mesh.h"
+#include "Platform/Graphics/RenderCommand.h"
+
+// This struct is a private implementation detail of the Renderer.
+// It holds data that is constant for an entire scene render pass.
+struct SceneData {
+    Matx4f View;
+};
+
+// We create a static instance of this data for the Renderer to use.
+// It is OK as a single scene is rendered at a time, so this is thread-safe.
+// NOT AN ATTRIBUTE OF RENDERER CLASS DUE TO SINGLETON PATTERN
+static SceneData s_SceneData;
+/*
+Public API implementation
+*/
+
+void Renderer::Init() {
+	RenderCommand::Init();
+}
+
+void Renderer::Shutdown() {
+}
+
+void Renderer::OnWindowResize(uint32_t width, uint32_t height) {
+    // Pass the viewport resize command down to the RenderCommand system.
+    RenderCommand::SetViewport(0, 0, width, height);
+}
+
+void Renderer::BeginScene(const Camera& camera) {
+    // At the beginning of rendering a scene, cache the camera's
+    // combined view and projection matrix. This will be used for
+    // every single object that gets drawn in this frame.
+    s_SceneData.View = camera.GetViewProjectionMatrix();
+}
+
+void Renderer::EndScene() {
+}
+
+void Renderer::Submit(
+    const std::shared_ptr<Mesh>& mesh,
+    const std::shared_ptr<Material>& material,
+    const Matx4f& transform
+) {
+    const auto& shader = material->GetShader();
+    const auto& vertexArray = mesh->GetVertexArray();
+    const auto& indexBuffer = mesh->GetIndexBuffer();
+
+    shader->Bind();
+
+    shader->SetUniformMat4f("u_ViewProjection", s_SceneData.View);
+    shader->SetUniformMat4f("u_Model", transform);
+
+	RenderCommand::Draw(vertexArray, indexBuffer);
+}
