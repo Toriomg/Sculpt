3b5b75afd9e7c1bebeec5ce1ee1ee7904cbf2804
Toriomg <hectorusuario@gmail.com>
2025-08-31 18:56:34 +0200

    Vec2 Modif

--- OpenGL/src/Platform/CoreUtils/Math/vec2.h
+++ OpenGL/src/Platform/CoreUtils/Math/vec2.h
@@ -18,13 +18,27 @@ class Vec2 {
 	Vec2 operator/(float scalar) const {
 		return Vec2(x / scalar, y / scalar);
 	}
-
+	Vec2 operator+=(const Vec2& other) {
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+	Vec2 operator-=(const Vec2& other) {
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+	Vec2 operator*=(float scalar) {
+		x *= scalar;
+		y *= scalar;
+		return *this;
+	}
 	Vec2 operator-() const {
 		return Vec2(-x, -y);
 	}
 
-	Vec2 dotProduct(const Vec2& other) const {
-		return Vec2(x * other.x, y * other.y);
+	float dotProduct(const Vec2& other) const {
+		return x * other.x + y * other.y;
 	}
 
 	float length() const {
