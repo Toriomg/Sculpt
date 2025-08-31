e1b6394f929be70a8c617b80a8829bfb94261238
Toriomg <hectorusuario@gmail.com>
2025-08-31 19:04:36 +0200

    Core folder copied, I have to implement scene into the editor layer

---
+++ OpenGL/src/Core/Entity.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <cstdint>
+#include "entt/entt.hpp" // The core ECS library
+
+// Using Entity as an alias for entt::entity
+using Entity = entt::entity;
+
+// Define a constant for a null/invalid entity
+const Entity NULL_ENTITY = entt::null;
+
